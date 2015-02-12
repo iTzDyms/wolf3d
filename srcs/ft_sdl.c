46503aeaae8ae2900a37a8ba7a0729a934976743
Cyril HERON <cheron@e1r4p13.42.fr>
2015-02-12 15:32:21 +0100

    pls

---
+++ srcs/ft_sdl.c
@@ -0,0 +1,41 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_sdl.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: cheron <cheron@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2015/01/22 14:05:20 by cheron            #+#    #+#             */
+/*   Updated: 2015/02/12 15:28:06 by cheron           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <SDL.h>
+#include <stdlib.h>
+#include <libft.h>
+
+static	void	ft_sdl_init(void)
+{
+	if (SDL_Init(SDL_INIT_VIDEO) == -1)
+	{
+		ft_putstr("Can't init SDL: ");
+		ft_putendl(SDL_GetError());
+		exit(1);
+	}
+}
+
+SDL_Window		*ft_createwin(char *title, int height, int width)
+{
+	SDL_Window *win;
+
+	ft_sdl_init();
+	win = SDL_CreateWindow(title, SDL_WINDOWPOS_CENTERED,
+		SDL_WINDOWPOS_CENTERED, width, height, SDL_WINDOW_SHOWN);
+	if (win == NULL)
+	{
+		ft_putstr("Can't set video mode: ");
+		ft_putendl(SDL_GetError());
+		exit(1);
+	}
+	return (win);
+}
